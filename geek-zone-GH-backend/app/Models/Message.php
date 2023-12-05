dea02d95224fbb348763816757e36c401e76eb13
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-05 19:19:57 +0100

    feat: added messages model without relations

---
+++ geek-zone-GH-backend/app/Models/Message.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $table = 'messages';
+
+    protected $fillable = [
+        'message',
+        'chat_id',
+        'user_id',
+    ];
+}
