a393be65e4c32dee8a79c493534b768f2428fd8c
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-05 17:47:01 +0100

    feat: added comments migration to database

--- geek-zone-GH-backend/database/migrations/2023_12_05_155428_create_comments_table.php
+++ geek-zone-GH-backend/database/migrations/2023_12_05_155428_create_comments_table.php
@@ -5,21 +5,21 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+{ 
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); 
+            $table->text("comment")->nullable(false);
+            $table->unsignedBigInteger("feed_id");
+            $table->foreign("feed_id")->references("id")->on("feeds")->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->constrained()->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+ 
     public function down(): void
     {
         Schema::dropIfExists('comments');
