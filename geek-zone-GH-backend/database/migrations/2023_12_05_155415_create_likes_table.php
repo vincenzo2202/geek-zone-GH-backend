d4a45916f5eecf49b949a8dc5681d3290f4cc677
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-05 17:38:24 +0100

    feat: added likes migration to database

--- geek-zone-GH-backend/database/migrations/2023_12_05_155415_create_likes_table.php
+++ geek-zone-GH-backend/database/migrations/2023_12_05_155415_create_likes_table.php
@@ -5,21 +5,20 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+{ 
     public function up(): void
     {
         Schema::create('likes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); 
+            $table->unsignedBigInteger("feed_id");
+            $table->foreign("feed_id")->references("id")->on("feeds")->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->constrained()->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+ 
     public function down(): void
     {
         Schema::dropIfExists('likes');
