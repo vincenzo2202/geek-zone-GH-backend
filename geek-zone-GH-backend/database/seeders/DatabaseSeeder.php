641c36ef839b745270a88ceeee45b0f381e6f74d
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2023-12-06 13:45:26 +0100

    feat: added message seeder to message table

--- geek-zone-GH-backend/database/seeders/DatabaseSeeder.php
+++ geek-zone-GH-backend/database/seeders/DatabaseSeeder.php
@@ -30,6 +30,10 @@ class DatabaseSeeder extends Seeder
         $this->call([
             Chat_userSeeder::class,
         ]);
+
+        $this->call([
+            MessageSeeder::class,
+        ]);
     }
 
 }
