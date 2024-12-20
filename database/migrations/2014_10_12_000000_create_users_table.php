63cac386d0ef22c98c9ead09bde1c864df75ec83
Vincenzo Donnarumma <vincenzodonnarumma22@gmail.com>
2024-12-21 00:04:03 +0100

    [FIX]: fix migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,16 +10,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100)->nullable(false); 
-            $table->string('last_name', 100)->nullable(false); 
+            $table->string('name', 100)->nullable(false);
+            $table->string('last_name', 100)->nullable(false);
             $table->string('email')->unique()->nullable(false);
             $table->string('password')->nullable(false);
             $table->string('city')->nullable(false);
             $table->integer('phone_number')->nullable(false);
             $table->string('photo', 255)->default("https://telefe-static2.akamaized.net/media/256103/usuario-sin-foto-perfil-whatsapp.jpg");
-            $table->enum('role',['user','admin','super_admin'])->default('user');
-            $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->enum('role', ['user', 'admin', 'super_admin'])->default('user');
+
+            $table->datetime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->datetime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
